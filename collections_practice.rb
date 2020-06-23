123470dfd00a904a466da43d0aaeb06da2b6a66b
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-23 23:24:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,6 +5,7 @@ end
 
 #this puts it in backwards number order
 def sort_array_desc(arrays)
+  #need to have to two variables in the pipes so to make it go in decending order
   arrays.sort! { |number, num| num <=> number}
   return arrays
 end
